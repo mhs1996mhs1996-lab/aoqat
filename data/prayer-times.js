c72e48ea4eb44d20704c6402301f3656669dd4ea
mhs1996mhs1996-lab <mhs1996mhs1996@gmail.com>
2026-09-10 23:18:54 +0300

    Load legacy preview cleanup before design carousel

--- data/prayer-times.js
+++ data/prayer-times.js
@@ -11,33 +11,39 @@ const prayerTimes = [];
 })();
 
 window.addEventListener("load", () => {
-    if (document.querySelector('script[data-second-design]')) return;
-    const script = document.createElement("script");
-    script.src = "js/second-design.js?v=3";
-    script.dataset.secondDesign = "true";
-    script.onload = () => {
-        const extraScript = document.createElement("script");
-        extraScript.src = "js/additional-designs.js?v=4";
-        extraScript.dataset.additionalDesigns = "true";
-        extraScript.onload = () => {
-            const nightScript = document.createElement("script");
-            nightScript.src = "js/night-design.js?v=1";
-            nightScript.dataset.nightDesign = "true";
-            nightScript.onload = () => {
-                const fiveScript = document.createElement("script");
-                fiveScript.src = "js/five-new-designs.js?v=1";
-                fiveScript.dataset.fiveNewDesigns = "true";
-                fiveScript.onload = () => {
-                    const exportScript = document.createElement("script");
-                    exportScript.src = "js/exact-export.js?v=6";
-                    exportScript.dataset.exactExport = "true";
-                    document.body.appendChild(exportScript);
+    const hideLegacy = document.createElement("script");
+    hideLegacy.src = "js/hide-legacy-design.js?v=1";
+    hideLegacy.dataset.hideLegacyDesign = "true";
+    hideLegacy.onload = () => {
+        if (document.querySelector('script[data-second-design]')) return;
+        const script = document.createElement("script");
+        script.src = "js/second-design.js?v=4";
+        script.dataset.secondDesign = "true";
+        script.onload = () => {
+            const extraScript = document.createElement("script");
+            extraScript.src = "js/additional-designs.js?v=5";
+            extraScript.dataset.additionalDesigns = "true";
+            extraScript.onload = () => {
+                const nightScript = document.createElement("script");
+                nightScript.src = "js/night-design.js?v=2";
+                nightScript.dataset.nightDesign = "true";
+                nightScript.onload = () => {
+                    const fiveScript = document.createElement("script");
+                    fiveScript.src = "js/five-new-designs.js?v=2";
+                    fiveScript.dataset.fiveNewDesigns = "true";
+                    fiveScript.onload = () => {
+                        const exportScript = document.createElement("script");
+                        exportScript.src = "js/exact-export.js?v=7";
+                        exportScript.dataset.exactExport = "true";
+                        document.body.appendChild(exportScript);
+                    };
+                    document.body.appendChild(fiveScript);
                 };
-                document.body.appendChild(fiveScript);
+                document.body.appendChild(nightScript);
             };
-            document.body.appendChild(nightScript);
+            document.body.appendChild(extraScript);
         };
-        document.body.appendChild(extraScript);
+        document.body.appendChild(script);
     };
-    document.body.appendChild(script);
+    document.body.appendChild(hideLegacy);
 });
