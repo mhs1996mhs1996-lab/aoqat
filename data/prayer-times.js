f01855783b88f303380d7fdf0c3c0c439d875f80
mhs1996mhs1996-lab <mhs1996mhs1996@gmail.com>
2026-09-11 10:34:41 +0300

    Refresh carousel after reducing to two designs

--- data/prayer-times.js
+++ data/prayer-times.js
@@ -29,7 +29,7 @@ window.addEventListener("load", () => {
                 nightScript.dataset.nightDesign = "true";
                 nightScript.onload = () => {
                     const fiveScript = document.createElement("script");
-                    fiveScript.src = "js/five-new-designs.js?v=3";
+                    fiveScript.src = "js/five-new-designs.js?v=4";
                     fiveScript.dataset.fiveNewDesigns = "true";
                     fiveScript.onload = () => {
                         const editScript = document.createElement("script");
@@ -41,7 +41,7 @@ window.addEventListener("load", () => {
                             toolsScript.dataset.backgroundToolsOrganizer = "true";
                             toolsScript.onload = () => {
                                 const exportScript = document.createElement("script");
-                                exportScript.src = "js/exact-export.js?v=8";
+                                exportScript.src = "js/exact-export.js?v=9";
                                 exportScript.dataset.exactExport = "true";
                                 document.body.appendChild(exportScript);
                             };
