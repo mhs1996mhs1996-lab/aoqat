0429922f7406acb2d5cb78729671a4ea3b52028c
mhs1996mhs1996-lab <mhs1996mhs1996@gmail.com>
2026-09-11 11:49:48 +0300

    Refresh font controls preserving design colors

--- data/prayer-times.js
+++ data/prayer-times.js
@@ -45,7 +45,7 @@ window.addEventListener("load", () => {
                                 footerScript.dataset.footerBackgroundOrganizer = "true";
                                 footerScript.onload = () => {
                                     const fontScript = document.createElement("script");
-                                    fontScript.src = "js/universal-font-controls.js?v=1";
+                                    fontScript.src = "js/universal-font-controls.js?v=2";
                                     fontScript.dataset.universalFontControls = "true";
                                     fontScript.onload = () => {
                                         const historyScript = document.createElement("script");
