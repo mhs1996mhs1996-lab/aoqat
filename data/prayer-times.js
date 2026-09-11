d47cb28e658e15ce60e94b7f943d4c40f33d2eeb
mhs1996mhs1996-lab <mhs1996mhs1996@gmail.com>
2026-09-11 17:22:43 +0300

    Refresh Office style color palette

--- data/prayer-times.js
+++ data/prayer-times.js
@@ -64,7 +64,7 @@ window.addEventListener("load", () => {
                                     footerScript.dataset.footerBackgroundOrganizer = "true";
                                     footerScript.onload = () => {
                                         const fontScript = document.createElement("script");
-                                        fontScript.src = "js/universal-font-controls.js?v=6";
+                                        fontScript.src = "js/universal-font-controls.js?v=7";
                                         fontScript.dataset.universalFontControls = "true";
                                         fontScript.onload = () => {
                                             const fontManagerScript = document.createElement("script");
